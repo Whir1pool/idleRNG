aaecb5da102e3b6c2a41a147e3f7e48ea37ed54c
Whir1pool <77311161+Whir1pool@users.noreply.github.com>
2024-12-29 10:55:37 +1000

    Update script.js
    Added the inventory and other gameData stats needed for future updates

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let playerData = {
+let gameData = {
 	luck: 1,
-	rollSpeed: 10, // in seconds
+	rollSpeed: 1, // in seconds
+    inventory: {},
 
 }
-
+let canRoll = true // Determines whether the roll button is avaialbe yet.
 let swords = [
     { name: "stick", chance: 0.5 },
     { name: "stone", chance: 0.34 },
@@ -16,16 +17,49 @@ const cumulativeChance = swords.reduce((acc, sword) => {
     acc.push({
         ...sword,
         cumulative: parseFloat((lastCumulative + sword.chance).toFixed(10)),
-    }); // Calculates the cumulative value for each sword, necessary for rolling
+    }); // Assigns a cumulative value for each sword
     return acc;
 }, []);
 
 
 function rollSword() {
-    const roll = Math.pow(Math.random(), 1 / playerData.luck); // Maybe balance this later,
+    if (!canRoll) {
+        return;
+    }
+    canRoll = false
+
+    // Determines the luck of the roll
+    const roll = Math.pow(Math.random(), 1 / gameData.luck); // Maybe balance this later,
     const chosenSword =
         cumulativeChance.find((sword) => roll <= sword.cumulative) ||
         cumulativeChance[cumulativeChance.length - 1]; // Determines which cumulative value it lies between and returns that sword
     document.getElementById('swordRolled').innerHTML = `You got a ${chosenSword.name} (${chosenSword.chance * 100}%)`;
     console.log(`You rolled ${roll}, and got: ${chosenSword.name}`);
+
+    // Adding sword to inventory
+    if (gameData.inventory[chosenSword.name]) {
+        gameData.inventory[chosenSword.name]++;
+    } else {
+        gameData.inventory[chosenSword.name] = 1;
+
+        // Creating the sword in the inventory if its rolled for the first time
+
+        const inventoryDiv = document.querySelector('.inventory');
+        const swordElement = document.createElement('p');
+        swordElement.id = `${chosenSword.name}Inventory`;
+        swordElement.textContent = `${chosenSword.name}: ${gameData.inventory[chosenSword.name]}`;
+        inventoryDiv.appendChild(swordElement);
+    }
+
+    //Update inventory count
+
+    const swordElement = document.getElementById(`${chosenSword.name}Inventory`);
+    swordElement.textContent = `${chosenSword.name}: ${gameData.inventory[chosenSword.name]}`;
+
+    // Disable the button for a period of time
+    setTimeout(() => {
+        canRoll = true;
+    }, 1000 / gameData.rollSpeed);
 }
+
+document.getElementById(`rollbtn`).addEventListener("click", rollSword)
